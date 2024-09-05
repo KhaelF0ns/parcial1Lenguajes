568cfee1e0169d1bd3eecebfb601b722be6ac693
Khaelf0ns <mhatt.fons@gmail.com>
2024-09-05 15:37:19 -0500

    Parcial

--- punto3/cuentaPalabras.c
+++ punto3/cuentaPalabras.c
@@ -3,22 +3,23 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_LINE_LENGTH 1024
-
+// Convierte una cadena a minúsculas
 void to_lowercase(char *str) {
     while (*str) {
-        *str = tolower((unsigned char) *str);
-        str++;
+        *str = tolower((unsigned char)*str);
+        ++str;
     }
 }
 
-int count_occurrences(const char *line, const char *keyword) {
+// Cuenta las ocurrencias de 'key' en 'line'
+int count_occurrences(const char *line, const char *key) {
     int count = 0;
-    const char *p = line;
+    const char *pos = line;
+    size_t keyword_len = strlen(key);
 
-    while ((p = strstr(p, keyword)) != NULL) {
-        count++;
-        p += strlen(keyword); 
+    while ((pos = strstr(pos, key)) != NULL) {
+        ++count;
+        pos += keyword_len; 
     }
 
     return count;
@@ -31,28 +32,38 @@ int main(int argc, char *argv[]) {
     }
 
     const char *filename = argv[1];
-    char keyword[256];
-    char line[MAX_LINE_LENGTH];
-    FILE *file = fopen(filename, "r");
+    char *key;
+    char *line = NULL;
+    size_t line_size = 0;
+    FILE *file;
     int total_count = 0;
 
-    strncpy(keyword, argv[2], sizeof(keyword) - 1);
-    keyword[sizeof(keyword) - 1] = '\0'; 
-    to_lowercase(keyword);
+    // Aloca memoria para la palabra clave
+    key = malloc(strlen(argv[2]) + 1);
 
+    // Copia la palabra clave y conviértela a minúsculas
+    strcpy(key, argv[2]);
+    to_lowercase(key);
+
+    // Abre el archivo
+    file = fopen(filename, "r");
     if (!file) {
         perror("Error al abrir el archivo");
+        free(key);
         return 1;
     }
 
-    while (fgets(line, sizeof(line), file)) {
+    // Lee y procesa cada línea
+    while (getline(&line, &line_size, file) != -1) {
         to_lowercase(line);
-        total_count += count_occurrences(line, keyword);
+        total_count += count_occurrences(line, key);
     }
 
+    free(line);
+    free(key);
     fclose(file);
 
-    printf("%s se repite %d veces en el texto.\n", argv[2], total_count);
+    printf("'%s' se repite %d veces en el texto.\n", argv[2], total_count);
 
     return 0;
 }
